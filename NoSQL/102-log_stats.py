ef49a4604ce4c1196aa7dcf83ddae4ec9850e8d9
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-05-09 20:46:51 +0400

    Create 102-log_stats.py

---
+++ NoSQL/102-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""102-log_stats"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    print(f"{collection.count_documents({})} logs")
+
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status} status check")
+
+    print("IPs:")
+    top_ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
