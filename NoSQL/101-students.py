7df1af4b1121a0cd4fe4f67bc66314a3ac6aa940
Abdullayev <70691153+Abdullayews@users.noreply.github.com>
2026-05-09 20:10:35 +0400

    Create 101-students.py

---
+++ NoSQL/101-students.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""101-students"""
+
+
+def top_students(mongo_collection):
+    return mongo_collection.aggregate([
+        {"$project": {
+            "name": 1,
+            "averageScore": {"$avg": "$topics.score"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ])
